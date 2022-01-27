cdf3cc3996519acd5b1f0ec088ffb4a4173e67d1
Alexandr <94461244+BMW-GT3@users.noreply.github.com>
2022-01-27 19:02:11 +0300

    PIN cheak для банка!!!
    PIN chaek для банка!!!

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let value = +prompt("@BMW.GT3, введите Ваш 4-значный PIN", '');
+
+switch (value){
+	case 1000:
+		alert('Скопируй и введи ссылку : https://bmw-gt3.github.io/bank/index101.html');
+	break;
+	default:
+		alert('Неверно! Обновите страницу');
+}s
---
+++ js/main1.js
@@ -0,0 +1,9 @@
+let value = +prompt("@Jack.3, введите Ваш 4-значный PIN", '');
+
+switch (value){
+	case 2708:
+		alert('Скопируй и введи ссылку : https://bmw-gt3.github.io/bank/index2021.html');
+	break;
+	default:
+		alert('Неверно! Обновите страницу');
+}s
